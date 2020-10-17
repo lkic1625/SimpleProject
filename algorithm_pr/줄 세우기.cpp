afa89c0829902e9a59e30e15269fd0b641de356f
lkic1625 <lkic1625@gmail.com>
2020-10-18 01:26:16 +0900

    solve 12865, 2252

--- algorithm_pr/줄 세우기.cpp
+++ algorithm_pr/줄 세우기.cpp
@@ -5,39 +5,42 @@
 
 using namespace std;
 
-const int MAX = 32001;
-int n, m, a, b, node[MAX];
-queue<int> v[MAX];
-queue<int> q;
+const int MAX = 32000 + 1;
+int n, m, a, b, indegree[MAX];
+queue<int> adj[MAX], q;
 
 int main() {
 
+	//input
 	cin >> n >> m;
 	for (int i = 0; i < m; i++) {
 		scanf("%d %d", &a, &b);
-		node[b]++;
-		v[a].push(b);
+		//a -> b
+		indegree[b]++;
+		//make adj list 
+		adj[a].push(b);
 	}
+	//init queue
 	for (int i = 1; i <= n; i++) {
-		if (!node[i]) q.push(i);
+		if (indegree[i] == 0) {//push to the queue when node's indegree == 0
+			q.push(i);
+		}
 	}
 	while (!q.empty()) {
-		int root = q.front();
-		q.pop();
-		printf("%d ", root);
-		while (!v[root].empty()) {
-			int child = v[root].front();
-			v[root].pop();
-			node[child]--;
-			if (!node[child]) {
+		//pop
+		int vertex = q.front(); q.pop();
+		//print sorted node sequence
+		printf("%d ", vertex);
+
+		while (!adj[vertex].empty()) {
+			//pop
+			int child = adj[vertex].front(); adj[vertex].pop();
+
+			//update child node's indegree
+			indegree[child]--;
+			if (indegree[child] == 0) {//push to the queue when node's indegree == 0
 				q.push(child);
 			}
 		}
 	}
-
-
-
-
-
-
 }
