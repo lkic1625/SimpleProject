bfd7f718e982dc565696cd37d6b2927fdc682840
lkic1625 <lkic1625@gmail.com>
2020-11-21 19:18:41 +0900

    solved 2887

--- algorithm_pr/BAEK15956.cpp
+++ algorithm_pr/BAEK15956.cpp
@@ -1,54 +1,58 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <queue>
+#include <cstdio>
 #include <algorithm>
-#include <cstring>
-#include <set>
-#include <map>
-#include <cmath>
-#include <utility>
-#include <list>
-#include <unordered_set>
-#include <assert.h>
+#include <string>
+
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
 
-struct bus {
-	int s, e, i;
-	bool operator< (bus& a) {
-		if (a.s == s) return e > a.e;
-		return s < a.s;
-	}
-};
+const int MAXN = 100001 * 2;
 
-int cmp(const bus& a, const bus& b) {
-	return a.i < b.i;
+string tmp, str;
+
+int A[MAXN];
+
+void manachers(string S, int N)
+{
+    int r = 0, p = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (i <= r)
+            A[i] = min(A[2 * p - i], r - i);
+        else
+            A[i] = 0;
+
+        while (i - A[i] - 1 >= 0 && i + A[i] + 1 < N && S[i - A[i] - 1] == S[i + A[i] + 1])
+            A[i]++;
+
+        if (r < i + A[i])
+        {
+            r = i + A[i];
+            p = i;
+        }
+    }
+}
+int main()
+{
+    cin >> tmp;
+
+    int len = tmp.size();
+
+    for (int i = 0; i < len; i++)
+    {
+        str += '#';
+        str += tmp[i];
+    }
+    str += '#';
+
+    manachers(str, str.size());
+
+    len = str.size();
+    int ans = -1;
+    for (int i = 0; i < len; i++)
+        cout << A[i] << " " << i << endl;
+
+    printf("%d", ans);
+
+    return 0;
 }
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0), cout.tie(0);
-	int N, M;
-	cin >> N >> M;
-	vector<bus> v(M);
-	int back = 0;
-	for (int i = 0; i < M; i++) {
-		cin >> v[i].s >> v[i].e;
-		v[i].i = i + 1;
-		if (v[i].s > v[i].e) {
-			back = max(back, v[i].e);
-			v[i].e += N;
-		}
-	}
-	sort(v.begin(), v.end());
-	deque<bus> dq;
-	for (int i = 0; i < M; i++)
-		if (dq.empty() || dq.back().e < v[i].e)
-			dq.push_back(v[i]);
-	while (!dq.empty() && dq.front().e <= back) dq.pop_front();
-	sort(dq.begin(), dq.end(), cmp);
-	for (bus b : dq) cout << b.i << ' ';
-	return 0;
-}
