299327bb38154965b03b18a97e287677418c7cdb
lkic1625 <lkic1625@gmail.com>
2020-12-13 15:14:00 +0900

    add 1184

--- algorithm_pr/소스23.cpp
+++ algorithm_pr/소스23.cpp
@@ -1,46 +1,29 @@
 #include<iostream>
+
 #include<vector>
-#include<algorithm>
-#include<queue>
+
 
 using namespace std;
 
-const int MAX = 32001;
-int n, m, a, b, node[MAX];
-bool visited[MAX];
-queue<int> v[MAX];
-queue<int> q;
+int backtracking(int num, vector<int> addition_chain) {
+
+	if (num == addition_chain.back()) {
+		for (auto iter : addition_chain) {
+			cout << iter;
+		}
+		return addition_chain.size();
+	}
+
+	for (int i = addition_chain.size() - 2; i > 0; i--) {
+		for (int j = i + 1; j < addition_chain.size(); j++) {
+			vector<int> temp = addition_chain;
+			temp.push_back(addition_chain[i] + addition_chain[j]);
+			backtracking(num, temp);
+		}
+	}
+}
+
 
 int main() {
 
-	cin >> n >> m;
-	for (int i = 0; i < m; i++) {
-		scanf("%d %d", &a, &b);
-		node[b]++;
-		v[a].push(b);
-	}
-	for (int i = 1; i <= n; i++) {
-		if (!node[i]) q.push(i);
-	}
-	while (!q.empty()) {
-		int root = q.front();
-		q.pop();
-		printf("%d ", root);
-		while (!v[root].empty()) {
-			int child = v[root].front();
-			v[root].pop();
-			node[child]--;
-			if (!node[child]) {
-				q.push(child);		
-			}
-			
-			
-		}
-	}
-	
-		
-	
-
-
-	
 }
