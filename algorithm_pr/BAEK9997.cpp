b8f76d8164733b18a878295307f0ed38897baca4
hammer <hammer@channel.io>
2021-01-24 00:34:30 +0900

    solve baek9997

--- algorithm_pr/BAEK9997.cpp
+++ algorithm_pr/BAEK9997.cpp
@@ -1,30 +1,46 @@
 #include<iostream>
 #include<string>
+#include<math.h>
+
+#define AND &
+#define OR |
 
 using namespace std;
 
 const int MAX = 26;
+const int ALPHABET_SIZE = 26;
+const int CONTAINED_ALL = (1 << ALPHABET_SIZE) - 1;
 
 int N;
-string words[MAX];
 int word_to_bits[MAX];
-long combination;
+string words[MAX];
 
-int traversal(int i, int bit) {
 
+int traversal(int i, int contained, int subset) {
+    if(i > N) return 0;
+    if(contained == CONTAINED_ALL){
+        return pow(2, N - i);
+    }
+
+    int contained_with_i = contained OR word_to_bits[i];
+    int subset_with_i = subset OR (1 << i);
+    return traversal(i + 1, contained_with_i, subset_with_i) + traversal(i + 1, contained, subset);
 }
 
 int main() {
 	cin >> N;
 	for (int i = 0; i < N; i++) {
+	    cin >> words[i];
 		for (char alphabet : words[i]) {
-			word_to_bits[i] |= 1 << (alphabet - 'A');
+			word_to_bits[i] = word_to_bits[i] OR 1 << (alphabet - 'A');
 		}
 	}
-	combination = (1 << (N + 1)) - 1;
-	for (int subset = combination; subset; (subset - 1) & combination) {
-		//2^26 TLE
-	}
+	int ans = traversal(0, 0, 0);
+	cout << ans;
+	//combination = (1 << N) - 1;
+	//for (int subset = combination; subset; subset = ((subset - 1) AND combination)) {
+        //TLE
+	//}
 
 
 
