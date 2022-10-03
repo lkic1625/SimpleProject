678cc620400e6d0d64b27794138a6825a6fabcae
lkic1625 <lkic1625@gmail.com>
2022-10-03 17:34:37 +0900

    Add Success info comment.

--- algorithm_pr/minimum-initial-energy-to-finish-tasks.cpp
+++ algorithm_pr/minimum-initial-energy-to-finish-tasks.cpp
@@ -1,3 +1,8 @@
+/**
+ * Runtime: 417 ms, faster than 98.16% of C++ online submissions for Minimum Initial Energy to Finish Tasks.
+ * Memory Usage: 93.3 MB, less than 81.25% of C++ online submissions for Minimum Initial Energy to Finish Tasks.
+ */
+
 #include <vector>
 #include <algorithm>
 
@@ -7,21 +12,15 @@ class Solution {
 public:
     int minimumEffort(vector<vector<int>>& tasks) {
         sort(tasks.begin(), tasks.end(), [](const vector<int>& lv, const vector<int>& rv) -> bool {
-            int lDiff = lv[1] - lv[0];
-            int rDiff = rv[1] - rv[0];
-
-            return lDiff != rDiff ? lDiff < rDiff : lv[1] < rv[1];
+            return lv[1] - lv[0] < rv[1] - rv[0];
         });
 
         int ans = 0;
         for (const auto &task: tasks) {
-            if (ans + task[0] < task[1]) {
-                ans = task[1];
-            } else {
-                ans += task[0];
-            }
+            ans = max(task[1], ans + task[0]);
         }
 
         return ans;
     }
 };
+
